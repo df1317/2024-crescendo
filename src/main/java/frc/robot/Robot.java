ca64c7192bf1740b508509404bef1ee7afdbebfa
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-01-29 20:33:00 -0500

    feat: added rumble

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.lib.CTREConfigs;
@@ -51,7 +52,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    m_robotContainer.m_XboxController.getHID().setRumble(RumbleType.kBothRumble, 0.0);
+  }
 
   @Override
   public void disabledPeriodic() {}
