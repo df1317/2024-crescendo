e0dc7467a6b6dc7ebcbba3ab36543cbed8bf03b6
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-02-20 18:45:26 -0500

    feat: convert limelight angle to encoder value

--- src/main/java/frc/robot/commands/SetArmValueAbsolute.java
+++ src/main/java/frc/robot/commands/SetArmValueAbsolute.java
@@ -7,22 +7,25 @@ import frc.robot.Constants;
 public class SetArmValueAbsolute extends Command {
     private ArmSubsystem m_ArmSubsystem;
     private double target_value;
+    private double encoderShooterZero;
 
     public SetArmValueAbsolute(ArmSubsystem ArmSub, double value) {
         m_ArmSubsystem = ArmSub;
-        target_value = value;
+        target_value = encoderShooterZero + (value / 360);
         addRequirements(ArmSub);
     }
 
     @Override
     public void initialize() {
-        m_ArmSubsystem.spinUp(Constants.ArmShooterConstants.Arm.Speed);
+        m_ArmSubsystem.spinUp(
+                Constants.ArmShooterConstants.Arm.Speed * Math.floor((target_value - m_ArmSubsystem.encoder.get())));
     }
 
     @Override
     public boolean isFinished() {
-        if (m_ArmSubsystem.encoder.get() >= Constants.ArmShooterConstants.Arm.EncoderMax || m_ArmSubsystem.encoder.get() <= Constants.ArmShooterConstants.Arm.EncoderMin  || m_ArmSubsystem.encoder.get() == target_value) {
-            m_ArmSubsystem.spinDown();
+        if ((m_ArmSubsystem.encoder.get() >= Constants.ArmShooterConstants.Arm.EncoderMax && target_value < 0)
+                || (m_ArmSubsystem.encoder.get() <= Constants.ArmShooterConstants.Arm.EncoderMin && target_value > 0)) {
+
             return true;
         }
 
