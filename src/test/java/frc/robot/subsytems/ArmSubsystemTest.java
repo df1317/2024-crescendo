f1a6a3c61f4b407c9b514d71a3f3f28467f2b9ff
Jason Garrett <jason.garrett@spidasoftware.com>
2024-03-11 19:59:21 -0400

    add limit switch

--- src/test/java/frc/robot/subsytems/ArmSubsystemTest.java
+++ src/test/java/frc/robot/subsytems/ArmSubsystemTest.java
@@ -1,10 +1,10 @@
 package frc.robot.subsytems;
 
 import frc.robot.Constants;
+import frc.robot.MockDigitalInput;
 import frc.robot.subsystems.ArmSubsystem;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
@@ -20,6 +20,7 @@ public class ArmSubsystemTest {
     static TalonSRXSimCollection motor0Sim;
     static TalonSRXSimCollection motor1Sim;
     static DutyCycleEncoderSim encoderSim;
+    static MockDigitalInput limitSwitch;
 
     @BeforeAll
     static void setupSpec() {
@@ -28,32 +29,59 @@ public class ArmSubsystemTest {
         motor0Sim = armSub.motor0.getSimCollection();
         motor1Sim = armSub.motor1.getSimCollection();
         encoderSim = new DutyCycleEncoderSim(armSub.encoder);  // throws exception if this is set up more than once
+        limitSwitch = new MockDigitalInput(armSub.limitSwitch);
+        armSub.limitSwitch = limitSwitch;
     }
 
     @Test
     public void spinUp() {
-        // arm encoder at minimum, speed is positive
+        // arm encoder at minimum, speed is positive, limit switch true
         encoderSim.set(Constants.ArmShooterConstants.Arm.EncoderMin);
+        limitSwitch.set(true);
         armSub.spinUp(0.5);
         sleep(100);
         // expect positive motor voltage
         assert(motor0Sim.getMotorOutputLeadVoltage() > 0);
 
-        // arm encoder less than minimum, speed is positive
+        // arm encoder less than minimum, speed is positive, limit switch true
         encoderSim.set(Constants.ArmShooterConstants.Arm.EncoderMin - 0.1);
+        limitSwitch.set(true);
         armSub.spinUp(0.5);
         sleep(100);
         // expect motor voltage to be zero
         assert(motor0Sim.getMotorOutputLeadVoltage() == 0);
 
-        // arm encoder greater than maximum, speed is positive
+        // arm encoder greater than maximum, speed is positive, limit switch true
         encoderSim.set(Constants.ArmShooterConstants.Arm.EncoderMax + 0.1);
+        limitSwitch.set(true);
         armSub.spinUp(0.5);
         sleep(100);
         // expect positive motor voltage -- bringing the arm back from having gone too far the other way
         assert(motor0Sim.getMotorOutputLeadVoltage() > 0);
 
-        
+        // arm encoder at minimum, speed is positive, limit switch false
+        encoderSim.set(Constants.ArmShooterConstants.Arm.EncoderMin);
+        limitSwitch.set(false);
+        armSub.spinUp(0.5);
+        sleep(100);
+        // expect zero motor voltage
+        assert(motor0Sim.getMotorOutputLeadVoltage() == 0);
+
+        // arm encoder less than minimum, speed is positive, limit switch false
+        encoderSim.set(Constants.ArmShooterConstants.Arm.EncoderMin - 0.1);
+        limitSwitch.set(false);
+        armSub.spinUp(0.5);
+        sleep(100);
+        // expect motor voltage to be zero
+        assert(motor0Sim.getMotorOutputLeadVoltage() == 0);
+
+        // arm encoder greater than maximum, speed is positive, limit switch false
+        encoderSim.set(Constants.ArmShooterConstants.Arm.EncoderMax + 0.1);
+        limitSwitch.set(false);
+        armSub.spinUp(0.5);
+        sleep(100);
+        // expect zero motor voltage
+        assert(motor0Sim.getMotorOutputLeadVoltage() == 0);
     }
 
     @Test
