de99a14c6e071cc10b182c7f2f550ddc21b02f09
whynotjava <124908702+whynotjava@users.noreply.github.com>
2024-03-23 13:13:00 -0400

    feat: addded pid to intake and shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -47,7 +47,7 @@ public final class Constants {
       public static final class Firing {
         public static final int MotorID0 = 01;
         public static final int MotorID1 = 02;
-        public static final double Speed = -0.75; // must be between -1 and 1
+        public static final double Speed = 1000; // must be between -1 and 1
         public static final double Duration = 1.25;
         public static final double ClearingDelay = 0.5;
       }
@@ -55,7 +55,7 @@ public final class Constants {
       public static final class Intake {
         public static final int angle = 66; // get by taking the angle of the arm at rest and adding the interior
                                             // shooter angle difference between arm and shooter
-        public static final double Speed = -0.5; // must be between -1 and 1
+        public static final double Speed = 500; // must be between -1 and 1
         public static final double ShootSpeed = -1;
         public static final int Timeout = 4;
       }
--- src/main/java/frc/robot/subsystems/FiringSubsystem.java
+++ src/main/java/frc/robot/subsystems/FiringSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -21,6 +23,16 @@ public class FiringSubsystem extends SubsystemBase {
     public SparkPIDController shooter1PID;
     public SparkPIDController intakePID;
 
+    public double intakeKp = 1;
+    public double intakeKi = 0;
+    public double intakeKd = 0;
+    public double intakeFF = 0;
+
+    public double shooterKp = 1;
+    public double shooterKi = 0;
+    public double shooterKd = 0;
+    public double shooterFF = 0;
+
     public FiringSubsystem() {
         shooter0 = new CANSparkMax(3, MotorType.kBrushless);
         shooter1 = new CANSparkMax(4, MotorType.kBrushless);
@@ -32,29 +44,46 @@ public class FiringSubsystem extends SubsystemBase {
         shooter0PID = shooter0.getPIDController();
         shooter1PID = shooter1.getPIDController();
         intakePID = intake.getPIDController();
+
+        intakePID.setReference(0, CANSparkBase.ControlType.kVelocity);
+
+        intakePID.setP(intakeKp);
+        intakePID.setI(intakeKi);
+        intakePID.setD(intakeKd);
+        intakePID.setFF(intakeFF);
+
+        shooter0PID.setP(shooterKp);
+        shooter0PID.setI(shooterKi);
+        shooter0PID.setD(shooterKd);
+        shooter0PID.setFF(shooterFF);
+
+        shooter1PID.setP(shooterKp);
+        shooter1PID.setI(shooterKi);
+        shooter1PID.setD(shooterKd);
+        shooter1PID.setFF(shooterFF);
     }
 
     public void spinUpShooter(double speed) {
         SmartDashboard.putString("Firing Status", "Firing Shooter");
-        shooter0.set(speed);
-        shooter1.set(speed);
+        shooter0PID.setReference(speed, ControlType.kVelocity);
+        shooter1PID.setReference(speed, ControlType.kVelocity);
     }
 
     public void spinDownShooter() {
         SmartDashboard.putString("Firing Status", "Spinning Down Shooter");
-        shooter0.stopMotor();
-        shooter1.stopMotor();
+        shooter0PID.setReference(0, ControlType.kVelocity);
+        shooter1PID.setReference(0, ControlType.kVelocity);
     }
 
     public void spinUpIntake(double speed) {
         SmartDashboard.putString("Firing Status", "Spin up Intake");
-        intake.set(speed);
-
+        intakePID.setReference(speed, CANSparkBase.ControlType.kVelocity);
     }
 
     public void spinDownIntake() {
         SmartDashboard.putString("Firing Status", "Spin down Intake");
-        intake.stopMotor();
+        intakePID.setReference(0, CANSparkBase.ControlType.kVelocity);
+
     }
 
     public void logVals() {
