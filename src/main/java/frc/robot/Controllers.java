ad4d04c7c110f39c25be80b55a24cb55a9b3a6c6
Rebekah-Garrett <rebekahlgarrett@gmail.com>
2024-03-29 13:26:25 -0400

    feat: named all of the buttons

--- src/main/java/frc/robot/Controllers.java
+++ src/main/java/frc/robot/Controllers.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 
 public class Controllers {
@@ -10,43 +11,52 @@ public class Controllers {
     public final CommandJoystick m_JoystickL = new CommandJoystick(1);
     public final CommandJoystick m_JoystickR = new CommandJoystick(2);
 
+    public final Trigger shooterButton = new Trigger(m_JoystickL.button(1));
+    public final Trigger intakeButton = new Trigger(m_JoystickR.button(1));
+    public final Trigger robotCentricButton = new Trigger(m_XboxController.leftBumper());
+    public final Trigger rightClimberButton = new Trigger(m_JoystickR.button(3));
+    public final Trigger leftClimberButton = new Trigger(m_JoystickL.button(3));
+    public final Trigger rightAutoAlignArmButton = new Trigger(m_JoystickR.button(4));
+    public final Trigger leftAutoAlignArmButton = new Trigger(m_JoystickL.button(4));
+    public final Trigger manualArmAimButton = new Trigger(m_JoystickR.button(2));
+
     public boolean shooterButtonState() {
-        boolean shooterButtonState = m_JoystickL.button(1).getAsBoolean();
+        boolean shooterButtonState = shooterButton.getAsBoolean();
         return shooterButtonState;
     }
 
     public boolean intakeButtonState() {
-        boolean intakeButtonState = m_JoystickR.button(1).getAsBoolean();
+        boolean intakeButtonState = intakeButton.getAsBoolean();
         return intakeButtonState;
     }
 
     public boolean robotCentricButtonState() {
-        boolean robotCentricButtonState = m_XboxController.leftBumper().getAsBoolean();
+        boolean robotCentricButtonState = robotCentricButton.getAsBoolean();
         return robotCentricButtonState;
     }
 
     public boolean rightClimberButtonState() {
-        boolean rightClimberButtonState = m_JoystickR.button(3).getAsBoolean();
+        boolean rightClimberButtonState = rightClimberButton.getAsBoolean();
         return rightClimberButtonState;
     }
 
     public boolean leftClimberButtonState() {
-        boolean leftClimberButtonState = m_JoystickL.button(3).getAsBoolean();
+        boolean leftClimberButtonState = leftClimberButton.getAsBoolean();
         return leftClimberButtonState;
     }
 
-    public boolean rightAutoAimArmButtonState() {
-        boolean rightAutoAimArmButtonState = m_JoystickR.button(4).getAsBoolean();
+    public boolean rightAutoAlignArmButtonState() {
+        boolean rightAutoAimArmButtonState = rightAutoAlignArmButton.getAsBoolean();
         return rightAutoAimArmButtonState;
     }
 
-    public boolean leftAutoAimButtonState() {
-        boolean leftAutoAimButtonState = m_JoystickL.button(4).getAsBoolean();
+    public boolean leftAutoAlignArmButtonState() {
+        boolean leftAutoAimButtonState = leftAutoAlignArmButton.getAsBoolean();
         return leftAutoAimButtonState;
     }
 
     public boolean manualArmAimButtonState() {
-        boolean manualArmAimButtonState = m_JoystickR.button(2).getAsBoolean();
+        boolean manualArmAimButtonState = manualArmAimButton.getAsBoolean();
         return manualArmAimButtonState;
     }
 
